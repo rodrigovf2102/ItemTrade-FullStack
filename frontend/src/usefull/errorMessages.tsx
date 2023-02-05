168fbf8a320133e01bb99130f203ee7ffb8eef74
Rodrigo <rodrigovf2102@gmail.com>
2023-02-05 13:58:02 -0300

    fix: error message of insuficient balance fixed

--- frontend/src/usefull/errorMessages.tsx
+++ frontend/src/usefull/errorMessages.tsx
@@ -24,8 +24,9 @@ export default function errorMessagesAll(error : any, setErrorMessage : React.Di
   if (error.response.statusText ==="Unauthorized") return setErrorMessage(["Seu Login expirou, refaça o login"]);
   if (error.response?.data?.clientVersion ==="4.9.0") return setErrorMessage(["Imagem muito grande, pegue outra..."]);
   if (error.response?.data === "InsuficientBalance") return setErrorMessage(["Saldo insuficiente..."]);
+  if (error.response?.data?.detail === "InsuficientBalance") return setErrorMessage(["Saldo insuficiente..."]);
   if (error.response?.data?.detail === "UserCantBuyFromHimself") return setErrorMessage(["Usuário nao pode comprar de si mesmo..."]);
-  setErrorMessage(["Erro desconhecido, tente mais tarde ou refaça o login..."]);
+  setErrorMessage(["Erro, tente mais tarde ou refaça o login..."]);
 }
 
 function messageTranslateInglesToPortuguese(messages : string[], setErrorMessage : React.Dispatch<React.SetStateAction<string[]>>) {
